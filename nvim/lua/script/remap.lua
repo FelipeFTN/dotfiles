3df30f989e0e90791000fc6d1c758233a2d50ef1
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-12-09 15:59:15 -0300

    ✨ new mapping

--- nvim/lua/script/remap.lua
+++ nvim/lua/script/remap.lua
@@ -2,5 +2,7 @@ local keymap = vim.api.nvim_set_keymap
 local ns = { noremap = true, silent = true }
 
 keymap('n', "<Leader>m", ":Explore<CR>", ns)
-keymap('n', "<Leader>n", ":NERDTreeFocus<CR>", ns)
-keymap('n', "<Leader>j", ":NERDTreeToggle<CR>", ns)
+keymap('n', "<Leader>n", ":Vexplore<CR>", ns)
+
+-- keymap('n', "<Leader>n", ":NERDTreeFocus<CR>", ns)
+-- keymap('n', "<Leader>j", ":NERDTreeToggle<CR>", ns)
