add27b5569a90adf6a7326e2fc052dd008dbe302
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-07-06 18:34:36 -0300

    🔧 rust analyzer

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -63,9 +63,6 @@ require('lspconfig')['tsserver'].setup{
 require('lspconfig')['rust_analyzer'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
-    settings = {
-      ["rust-analyzer"] = {}
-    }
 }
 require('lspconfig')['clangd'].setup{
 	on_attach = on_attach,
