d19c698919dda89ed6b00db602db8ae2dc921290
FelipeFTN <felipe.tenorio123456@gmail.com>
2024-01-09 23:48:00 -0300

    🦀 Rust Analyzer

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "asm_lsp", "terraformls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "asm_lsp", "terraformls", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
