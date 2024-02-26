93d5f532b6cfac40cdeaa6cefde9d75bee84904a
FelipeFTN <felipe.tenorio123456@gmail.com>
2024-02-26 12:46:34 -0300

    🔉 better music script

--- polybar/.config/polybar/music.sh
+++ polybar/.config/polybar/music.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# NOTE: You need the "playerctl" pachage in order for this to work!!!
+# NOTE: You need the "playerctl" package in order for this to work!!!
 
 exec 2>/dev/null
 
 if [ "$(playerctl status)" = "Playing" ]; then
   title=`exec playerctl metadata xesam:title`
   artist=`exec playerctl metadata xesam:artist`
+
+  # Check if the length of artist is greater than 15 characters
+  if [ ${#artist} -gt 15 ]; then
+    artist="${artist:0:15}..." # Truncate if it's longer
+  fi
+
   echo "[$artist] $title"
 else
   echo "No song currently playing"
