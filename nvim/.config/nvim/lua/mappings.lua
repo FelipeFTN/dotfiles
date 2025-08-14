e418328eaa2c91a44aff7f3d5297f82b5f338f4b
FelipeFTN <FelipeFTN@protonmail.com>
2025-08-14 15:49:41 -0300

    nvim: better mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,7 +1,5 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
@@ -18,4 +16,24 @@ map("n", "<leader>gf", "<cmd> GoFmt <cr>", { desc = "Format Go file" })
 map("n", "<leader>gt", "<cmd> GoTest <cr>", { desc = "Run Go tests" })
 map("n", "<leader>gv", "<cmd> GoVet <cr>", { desc = "Run Go vet" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- LSP
+map("n", "<leader>lr", function()
+  vim.lsp.buf.references()
+end, { desc = "LSP references" })
+
+map("n", "<leader>ld", function()
+  vim.diagnostic.open_float { border = "rounded" }
+end, { desc = "LSP diagnostics" })
+
+map("n", "<leader>lD", function()
+  vim.diagnostic.setloclist()
+end, { desc = "LSP diagnostics in loclist" })
+
+map("n", "<leader>lf", function()
+  vim.diagnostic.open_float { border = "rounded" }
+end, { desc = "Floating diagnostic" })
+
+map("n", "<leader>ca", function()
+  vim.lsp.buf.code_action()
+end, { desc = "LSP code action" })
+
