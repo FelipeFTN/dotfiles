ca6d1e7d8b715118c54fff7f6625b325136ee4cc
Felipe Tenorio <felipeftn@protonmail.com>
2025-11-02 00:06:01 -0300

    Zsh plugins auto-installer

---
+++ zsh_plugins.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
